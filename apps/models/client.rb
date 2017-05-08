1bc5df13aafa34383a4815c5ddd0ecdc57f1b649
mahaffey <ryan.a.mahaffey@gmail.com>
2017-05-08 16:22:12 -0400

    Added initialize to Client Model

--- apps/models/client.rb
+++ apps/models/client.rb
@@ -2,4 +2,8 @@ class Client < ActiveRecord::Base
   has_many :trips
   has_many :drivers, through: :trips
   has_many :vehicles, through: :trips
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
