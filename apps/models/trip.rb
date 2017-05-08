f26e72158131f4b2a60fc3d9519caf6580e354a8
mahaffey <ryan.a.mahaffey@gmail.com>
2017-05-08 17:51:24 -0400

    added some additional methods, client vehicle and driver accessors to trip model. Initialize is not currently working in the reake console. let's debug!!

--- apps/models/trip.rb
+++ apps/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ActiveRecord::Base
-  attr_accessor :price, :miles, :num_of_pass, :pickup_time, :pickup_loc, :dropoff_time, :dropoff_loc
+  attr_accessor :price, :miles, :num_of_pass, :pickup_time, :pickup_loc, :dropoff_time, :dropoff_loc, :driver, :vehicle, :client
   attr_reader :driver_id, :vehicle_id, :client_id
 
   belongs_to :client
@@ -12,9 +12,13 @@ class Trip < ActiveRecord::Base
 
   def initialize(attributes,client_name)
     attributes.each { |key, value| self.send(("#{key}="), value) }
-    Client.find_or_create_by(client_name)
+    #Client.find_or_create_by(client_name)
     @@all << self
     @@active_trips << self
+  end
+
+  def add_or_update(attributes)
+    attributes.each { |key, value| self.send(("#{key}="), value) }
   end
 
   def self.all
@@ -29,4 +33,7 @@ class Trip < ActiveRecord::Base
     @@archived_trips
   end
 
+  # def set_driver=(driver)
+  #   self.driver = driver
+  # end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 require 'bundler'
 Bundler.require
+require_relative '../apps/models/client.rb'
+require_relative '../apps/models/driver.rb'
+require_relative '../apps/models/trip.rb'
+require_relative '../apps/models/vehicle.rb'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
