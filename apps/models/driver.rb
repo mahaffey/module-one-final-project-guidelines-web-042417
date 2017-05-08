996107e36d7a1486ead7617408246da971107385
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-05-08 16:22:12 -0400

    Create initialization

--- apps/models/driver.rb
+++ apps/models/driver.rb
@@ -2,4 +2,12 @@ class Driver < ActiveRecord::Base
   has_many :trips
   has_many :clients, through: :trips
   has_many :vehicles, through: :trips
+
+  @@all = []
+
+  def initialize(attributes)
+    attributes.each { |key, value| self.send(("#{key}="), value) }
+    @@all << self
+  end
+
 end
