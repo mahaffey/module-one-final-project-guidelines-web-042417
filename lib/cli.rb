13b4620bef6f72d1986355917874615c457f60b2
mahaffey <ryan.a.mahaffey@gmail.com>
2017-05-10 15:31:06 -0400

    Started the modify and view options menus and methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,14 +8,14 @@ class CLI
   def options
     puts "What would you like to do? (select 1-3)"
     puts "1. Schedule a new trip"
-    puts "2. Modify an existing trip"
+    puts "2. Update or View an existing trip"
     puts "3. Quit"
 
     case gets.strip
     when "1"
       schedule_new_trip
     when "2"
-      #modify_existing_trip
+      modify_or_view_existing_trip
     when "3"
       exit
     else
@@ -27,9 +27,7 @@ class CLI
   def schedule_new_trip
     puts "Enter client full name:"
     name = gets.strip.split(" ")
-
     client = Client.find_by({first_name: name[0], last_name: name[1]})
-
     client = Client.create(get_client_attributes(name)) if client == nil
     new_trip = Trip.create(get_trip_attributes)
     new_trip.client = client
@@ -53,15 +51,6 @@ class CLI
     puts "-------------------------------------------------"
   end
 
-
-  #refactored with seperator_and_text - delete
-  # def please_input_info
-  #   puts "-------------------------------------------------"
-  #   puts "Please input the following information:"
-  #   puts "(if information is unavailible simply hit return)"
-  #   puts "-------------------------------------------------"
-  # end
-
   def get_client_attributes(name)
     attributes = {first_name: name[0], last_name: name[1],
                   company: "Client company" , phone: "Clients phone number", email: "Client's email address",
@@ -74,7 +63,6 @@ class CLI
       puts "#{value}:"
       attributes[key] = gets.strip
     end
-
     attributes
   end
 
@@ -88,50 +76,8 @@ class CLI
       puts "#{value}:"
       attributes[key] = gets.strip
     end
-
-    #refactor example - delete
-    # puts "How many passengers:"
-    # attributes[:num_of_pass] = gets.strip.to_i
-    # puts "Pick up time:"
-    # attributes[:pickup_time] = gets.strip
-    # puts "Pick up address: (Street, Zip)"
-    # attributes[:pickup_loc] = gets.strip
-    # puts "Drop off address: (Street, Zip)"
-    # attributes[:dropoff_loc] = gets.strip
-
-
     attributes
   end
-
-  #2 Options
-    # Schedule a trip (create)
-      #insert from 3
-    # 7 Modify a trip (update)
-    # 8 Quit App
-
-    #3 create/retrieve client by name
-      #if client does not exist
-        #4 get client attributes
-          #5a create client
-      # if client exists
-        #5b find client by name
-
-      #6 initialize create a trip with found/created client
-      #6.5 give user trip ID
-
-    # ---- COMING SOON TO AN APP NEAR YOU -----
-
-    #directions = GoogleDirections.new(origin, destination)
-    #drive_time_in_minutes = directions.drive_time_in_minutes
-    #distance_in_miles = directions.distance_in_miles
-    #directions.methods
-    #directions.distance = meters
-    #directions.distance_text.split(" ")[0].to_f
-      # create driver (create)
-        # request driver
-      # create vehicle (create)
-        # request vehicle
-          #attributes???
 
   def driver_prompt
     puts "Enter new driver's information:"
@@ -141,7 +87,6 @@ class CLI
     puts "Driver name: #{new_driver.name}"
   end
 
-
   def get_driver_attributes_and_create
     attributes = {
       name: "Name of driver", age: "Age of driver",
@@ -150,14 +95,11 @@ class CLI
       address: "Driver's home address", lic_state: "License state",
       lic_number: "License number", lic_class: "License class"
     }
-
    attributes.collect do |key, value|
      puts "#{value}:"
      attributes[key] = gets.strip
    end
-
    Driver.create(attributes)
-
   end
 
   def vehicle_prompt
@@ -167,7 +109,6 @@ class CLI
     puts "New Vehicle ID: ##{vehicle_new.id}"
   end
 
-
   def get_vehicle_attributes_and_create
     attributes = {
       lic_plate: "License plate number", year: "Year of vehicle",
@@ -175,14 +116,82 @@ class CLI
       seats: "How many seats", mileage: "Current mileage",
       type: "Type of vehicle", veh_class: "License class required to drive"
     }
+   attributes.collect do |key, value|
+     puts "#{value}:"
+     attributes[key] = gets.strip
+   end
+   Vehicle.create(attributes)
+ end
+
+ def get_trip_id
+    seperator_and_text {puts "Enter the trip ID"}
+    gets.strip.to_i
+  end
+
+ def update_or_view_existing_trip
+    seperator_and_text {puts "What would you like to do? (select 1-3)" puts "1. Update an existing trip" puts "2. View an existing trip" puts "3. Back to main menu"}
+    case gets.strip
+    when "1"
+      update_trip
+    when "2"
+      view_trip
+    when "3"
+      options
+    else
+      puts "Invalid Input: Please Enter 1-3"
+      update_or_view_existing_trip
+    end
+ end
+
+ def updated_attributes
+   attributes = {num_of_pass: "How many passengers", pickup_time: "Pick up time", pickup_loc: "Pick up address: (Street, Zip)", dropoff_loc: "Drop off address: (Street, Zip)"}
+
+   seperator_and_text {puts "Please input the following information:"
+                       puts "(if information is unavailible simply hit return)"}
 
    attributes.collect do |key, value|
      puts "#{value}:"
      attributes[key] = gets.strip
    end
-
-   Vehicle.create(attributes)
-
+   attributes
  end
+
+ def update_trip
+   Trip.all.where("ID = #{get_trip_id}").update(updated_attributes)
+ end
+
+ def view_trip
+   Trip.all[get_trip_id-1]
+ end
+
+ #2 Options
+   # Schedule a trip (create)
+     #insert from 3
+   # 7 Modify a trip (update)
+   # 8 Quit App
+
+   #3 create/retrieve client by name
+     #if client does not exist
+       #4 get client attributes
+         #5a create client
+     # if client exists
+       #5b find client by name
+
+     #6 initialize create a trip with found/created client
+     #6.5 give user trip ID
+
+   # ---- COMING SOON TO AN APP NEAR YOU -----
+
+   #directions = GoogleDirections.new(origin, destination)
+   #drive_time_in_minutes = directions.drive_time_in_minutes
+   #distance_in_miles = directions.distance_in_miles
+   #directions.methods
+   #directions.distance = meters
+   #directions.distance_text.split(" ")[0].to_f
+     # create driver (create)
+       # request driver
+     # create vehicle (create)
+       # request vehicle
+         #attributes???
 
 end
