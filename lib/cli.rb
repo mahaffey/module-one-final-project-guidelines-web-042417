a0e5b8614a869eff349fa911b6961162883b0edc
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-05-10 13:17:03 -0400

    combined driver and vehicle methods into main cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -135,4 +135,56 @@ class CLI
         # request vehicle
           #attributes???
 
+  def driver_prompt
+    puts "Enter new driver's information:"
+    new_driver = get_driver_attributes_and_create
+    puts "A new driver has been created"
+    puts "Driver ID number: #{new_driver.id}"
+    puts "Driver name: #{new_driver.name}"
+  end
+
+
+  def get_driver_attributes_and_create
+    attributes = {
+      name: "Name of driver", age: "Age of driver",
+      experience: "Years of experience driving",
+      phone: "Driver phone number", email: "Driver's email address",
+      address: "Driver's home address", lic_state: "License state",
+      lic_number: "License number", lic_class: "License class"
+    }
+
+   attributes.collect do |key, value|
+     puts "#{value}:"
+     attributes[key] = gets.strip
+   end
+
+   Driver.create(attributes)
+
+  end
+
+  def vehicle_prompt
+    puts "Enter new vehicle's information:"
+    vehicle_new = get_vehicle_attributes_and_create
+    puts "A new vehicle has been added to the garage"
+    puts "New Vehicle ID: ##{vehicle_new.id}"
+  end
+
+
+  def get_vehicle_attributes_and_create
+    attributes = {
+      lic_plate: "License plate number", year: "Year of vehicle",
+      make: "Make", model: "Model", color: "Color",
+      seats: "How many seats", mileage: "Current mileage",
+      type: "Type of vehicle", veh_class: "License class required to drive"
+    }
+
+   attributes.collect do |key, value|
+     puts "#{value}:"
+     attributes[key] = gets.strip
+   end
+
+   Vehicle.create(attributes)
+   
+ end
+
 end
